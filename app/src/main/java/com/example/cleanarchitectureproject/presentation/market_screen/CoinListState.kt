feae65615c8fe836ee7de11cd5e79993dc46b258
Swapnil <swapnilp0501@gmail.com>
2025-01-31 11:29:53 +0530

    feat: Added CoinListState.

--- app/src/main/java/com/example/cleanarchitectureproject/presentation/market_screen/CoinListState.kt
+++ app/src/main/java/com/example/cleanarchitectureproject/presentation/market_screen/CoinListState.kt
@@ -1,4 +1,9 @@
 package com.example.cleanarchitectureproject.presentation.market_screen
 
-class CoinListState {
-}
+import com.example.cleanarchitectureproject.data.remote.dto.coinmarket.CryptocurrencyCMDTO
+
+data class CoinListState (
+    val isLoading: Boolean =false,
+    val cryptocurrency: CryptocurrencyCMDTO?= null,
+    val error: String = ""
+)
--- app/src/main/java/com/example/cleanarchitectureproject/presentation/market_screen/MarketViewModel.kt
+++ app/src/main/java/com/example/cleanarchitectureproject/presentation/market_screen/MarketViewModel.kt
@@ -8,7 +8,6 @@ import androidx.lifecycle.viewModelScope
 import com.example.cleanarchitectureproject.common.Resource
 import com.example.cleanarchitectureproject.data.remote.dto.coinmarket.CryptoCurrencyCM
 import com.example.cleanarchitectureproject.domain.use_case.get_currency_stats.GetCurrencyStatsUseCase
-import com.example.cleanarchitectureproject.presentation.home_screen.CoinStatsState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -22,8 +21,8 @@ class MarketViewModel @Inject constructor(
     private val getCurrencyStatsUseCase: GetCurrencyStatsUseCase,
 ) : ViewModel() {
 
-    private val _coinList = mutableStateOf(CoinStatsState())
-    val coinList: State<CoinStatsState> = _coinList
+    private val _coinList = mutableStateOf(CoinListState())
+    val coinList: State<CoinListState> = _coinList
 
     init {
         getCoinStats()
@@ -33,18 +32,18 @@ class MarketViewModel @Inject constructor(
         getCurrencyStatsUseCase().onEach { result ->
             when (result) {
                 is Resource.Success -> {
-                    _coinList.value = CoinStatsState(cryptocurrency = result.data)
+                    _coinList.value = CoinListState(cryptocurrency = result.data)
                     Log.d("cryptoCurrency", "getCoin: ${result.data}")
                 }
 
                 is Resource.Error -> {
-                    _coinList.value = CoinStatsState(
+                    _coinList.value = CoinListState(
                         error = result.message ?: "An unexpected error occured"
                     )
                 }
 
                 is Resource.Loading -> {
-                    _coinList.value = CoinStatsState(isLoading = true)
+                    _coinList.value = CoinListState(isLoading = true)
                 }
             }
         }.launchIn(viewModelScope)
