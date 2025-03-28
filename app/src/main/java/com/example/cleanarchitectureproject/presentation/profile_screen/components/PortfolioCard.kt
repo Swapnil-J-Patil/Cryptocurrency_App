b9aef0f9521ace758a002c3ee85dc1fac81b9364
Swapnil <swapnilp0501@gmail.com>
2025-03-28 10:21:26 +0530

    feat: Added filter UI in the portfolioScreen.

--- app/src/main/java/com/example/cleanarchitectureproject/presentation/profile_screen/components/PortfolioCard.kt
+++ app/src/main/java/com/example/cleanarchitectureproject/presentation/profile_screen/components/PortfolioCard.kt
@@ -5,19 +5,27 @@ import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FilterList
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -27,6 +35,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.cleanarchitectureproject.data.remote.dto.coinmarket.CryptoCurrencyCM
 import com.example.cleanarchitectureproject.domain.model.CryptocurrencyCoin
@@ -104,11 +113,32 @@ fun PortfolioCard(
         ) {
 
             item {
-                Box(
-                    modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.CenterEnd
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(start = 5.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 )
                 {
+                    Row(
+                        modifier = Modifier.clickable {
+
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.FilterList,
+                            contentDescription = "FilterCoins",
+                            modifier = Modifier.size(30.dp),
+                            tint = MaterialTheme.colorScheme.secondary
+                        )
+                        Spacer(modifier = Modifier.width(5.dp))
+                        Text(
+                            text = "Filters",
+                            color = MaterialTheme.colorScheme.secondary,
+                            style = MaterialTheme.typography.titleMedium,
+                            fontWeight = FontWeight.Medium
+                        )
+                    }
                     CoinDisplay(amount = dollars.toDouble(), isSell = false)
                 }
             }
@@ -128,9 +158,7 @@ fun PortfolioCard(
                 Spacer(modifier = Modifier.height(20.dp))
 
             }
-            /*item {
-                Spacer(modifier = Modifier.height(40.dp))
-            }*/
+
             itemsIndexed(
                 portfolioCoins,
                 key = { _, coin -> coin.id }) { index, portfolioCoin ->  // Use key
